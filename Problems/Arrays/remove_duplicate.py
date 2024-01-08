ce3b09306c1f8ac73c64338fcd636b005a475821
Ravi Kumar <ravi26067@gmail.com>
2024-01-08 23:58:01 +0530

    remove duplicate

---
+++ Problems/Arrays/remove_duplicate.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        num_set = set()
+        i = 0
+
+        while i < len(nums):
+            if nums[i] not in num_set:
+                num_set.add(nums[i])
+                i += 1
+            else:
+                nums.pop(i)
+
+        return len(nums)
+
