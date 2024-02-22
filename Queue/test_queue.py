edd0dc36a0c22e77ed0ce1edc3e3892e38fcae0e
Ravi Kumar <ravi26067@gmail.com>
2024-02-22 17:05:00 +0530

    Test for github actions

--- Queue/test_queue.py
+++ Queue/test_queue.py
@@ -6,7 +6,7 @@ def test_enqueue():
     q.enQueue(5)
     q.enQueue(10)
     q.enQueue(1)
-    assert q.s1 == [5, 10, 1]
+    assert q.s1 == [1, 10, 5]
 
 
 def test_dequeue():
@@ -15,22 +15,9 @@ def test_dequeue():
     q.enQueue(10)
     q.enQueue(1)
     assert q.deQueue() == 5
-    assert q.s1 == [10, 1]
+    assert q.s1 == [1, 10]
 
 
 def test_dequeue_empty_queue():
     q = queue_using_stack.Queue()
     assert q.deQueue() == -1
-
-
-def test_print_queue():
-    q = queue_using_stack.Queue()
-    q.enQueue(5)
-    q.enQueue(10)
-    q.enQueue(1)
-    assert q.printQueue() == "Queue: [5, 10, 1]"
-
-
-def test_print_empty_queue():
-    q = queue_using_stack.Queue()
-    assert q.printQueue() == "Queue is empty"
